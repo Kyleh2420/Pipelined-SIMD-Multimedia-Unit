1a6d8dcddec3bb02ee851b379cc1667339e974e6
Kyle Han <kyleh2420@gmail.com>
2023-10-29 14:57:50 -0400

    Added longMulSubHi. Fixed some bugs

--- alu_tb.vhd
+++ alu_tb.vhd
@@ -21,7 +21,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use work.all;
+use work.all; 
+use std.env.finish;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -234,6 +235,43 @@ begin
 			
 			wait for 10 ns;
 			
+			--Testing for R4 Instruction: intMulAddHi
+			-- wordIn is 1 0 001 00000 00000 00000 00000
+			
+			--0th
+			--Set the 0th set of 32 bits of rs1 to 3  
+			--Set the 0th set of 32 bits of rs2 to 5 
+			--Set the 0th set of 32 bits of rs3 to 5
+			--That is: (5*5) + 3 = 28, which is 0x0_001C
+			
+			--1st
+			--Testing the overflow portion
+			--Set the 1st set of 32 bits of rs1 to 0x7FFF_FFFC, the hightest 32 bit number - 3
+			--Set the 1st set of 16 bits of rs2 to 0x7FFF, the highest 16 bit 
+			--Set the 1st set of 16 bits of rs3 to 0x0002, 2
+			--That is: (32,767*2) = 65,534 + 4,294,967,296 = 4,295,032,830, which overflowed
+			--Therefore, saturation should set it at 0x7FFF_FFFF
+			
+			--2nd
+			--Testing the normal operation
+			--Set the 1st set of 32 bits of rs1 to 0x1000_0000, the lowest 32 bit number
+			--Set the 1st set of 16 bits of rs2 to 0x0000, 0 
+			--Set the 1st set of 16 bits of rs3 to 0x0000, 0
+			--That is: (0*0) = 0 - 4,294,967,296 = -4,294,967,296, or 0x1000_0000
+			
+			--3rd
+			--Testing the normal operation
+			--Set the 1st set of 32 bits of rs1 to 0x7FFF_FFFF, the highest 32 bit number
+			--Set the 1st set of 16 bits of rs2 to 0x8000, The lowest 16 bit number 
+			--Set the 1st set of 16 bits of rs3 to 0x7FFF, The Highest 16 bit number
+			--That is: (32,767*-32,767) = -1,073,676,289 + 4,294,967,296 = ______
+			wordIn <= "1000100000000000000000000";
+			rs1 <= X"7FFFFFFF100000007FFFFFFC00000003";
+			rs2 <= X"80000000000000007FFF000000050000";
+			rs3 <= X"FFFF0000000000000002000000050000"; 
+			
+			wait for 10 ns;
+			
 			--Testing for R4 Instruction: intMulSubLo
 			-- wordIn is 1 0 010 00000 00000 00000 00000
 			
@@ -271,141 +309,6 @@ begin
 			rs1 <= X"80000000800000007FFFFFFF00000003";
 			rs2 <= X"00007FFF00007FFF0000000100000005";
 			rs3 <= X"00007FFF00007FFF0000800000000005"; 
-			
-			wait for 10 ns;
-			
-			--Testing for R4 Instruction: longMulAddLo
-			-- wordIn is 1 0 100 00000 00000 00000 00000
-			
-			--0th
-			--Overflow Operation
-			--Set the 0th set of 64 bits of rs1 to 0x7FFF_FFFF_FFFF_FFFF, the highest 64 bit number 
-			--Set the 0th set of 32 bits of rs2 to 0x7FFF_FFFF 
-			--Set the 0th set of 32 bits of rs3 to 0x7FFF_FFFF
-			--This should cap out at 0x7FFF_FFFF_FFFF_FFFF
-			
-			--1st
-			--Testing the Underflow portion
-			--Set the 1st set of 32 bits of rs1 to 0x8000_0000_0000_0000, the lowest 64 bit number
-			--Set the 1st set of 16 bits of rs2 to 0x8000_0000, The lowest 32 bit number
-			--Set the 1st set of 16 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
-			--This should saturate at 0x8000_0000_0000_0000
-			wordIn <= "1010000000000000000000000";
-			rs1 <= X"80000000000000007FFFFFFFFFFFFFFF";
-			rs2 <= X"0000000080000000000000007FFFFFFF";
-			rs3 <= X"000000007FFFFFFF000000007FFFFFFF"; 
-			
-			wait for 10 ns;
-			
-			--Testing for R4 Instruction: longMulAddLo
-			-- wordIn is 1 0 100 00000 00000 00000 00000
-			
-			--0th
-			--Normal Operation
-			--Set the 1st set of 64 bits of rs1 to 0x0000_0000_000F_4240, the lowest 64 bit number
-			--Set the 1st set of 32 bits of rs2 to 0x7FFF_FFFF, The highest 32 bit number
-			--Set the 1st set of 32 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
-			--That is: (2147483647 * 2147483647) + 1000000 = 4,611,686,014,133,420,609
-			--Expect: 3FFFFFFF000F4241
-			
-			--1st
-			--Normal Operation
-			--Set the 1st set of 64 bits of rs1 to 0x0000_0000_000F_4240, the lowest 64 bit number
-			--Set the 1st set of 32 bits of rs2 to 0x7FFF_FFFF, The highest 32 bit number
-			--Set the 1st set of 32 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
-			--That is: (2147483647 * 2147483647) + 1000000 = 4,611,686,014,133,420,609
-			--Expect: 3FFFFFFF000F4241
-			wordIn <= "1010000000000000000000000";
-			rs1 <= X"00000000000F424000000000000F4240";
-			rs2 <= X"000000007FFFFFFF000000007FFFFFFF";
-			rs3 <= X"000000007FFFFFFF000000007FFFFFFF"; 
-			
-			wait for 10 ns;
-			
-			--Testing for R4 Instruction: longMulSubLo
-			-- wordIn is 1 0 110 00000 00000 00000 00000
-			
-			--OVERFLOW AND UNDERFLOW OF THIS NEEDS TO BE DONE
-			
-			--0th
-			--Overflow Operation
-			--Set the 0th set of 64 bits of rs1 to 0x7FFF_FFFF_FFFF_FFFF, the highest 64 bit number 
-			--Set the 0th set of 32 bits of rs2 to 0x7FFF_FFFF 
-			--Set the 0th set of 32 bits of rs3 to 0x7FFF_FFFF
-			--This should cap out at 0x7FFF_FFFF_FFFF_FFFF
-			
-			--1st
-			--Testing the Underflow portion
-			--Set the 1st set of 32 bits of rs1 to 0x8000_0000_0000_0000, the lowest 64 bit number
-			--Set the 1st set of 16 bits of rs2 to 0x8000_0000, The lowest 32 bit number
-			--Set the 1st set of 16 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
-			--This should saturate at 0x8000_0000_0000_0000
-			wordIn <= "1011000000000000000000000";
-			rs1 <= X"80000000000000007FFFFFFFFFFFFFFF";
-			rs2 <= X"0000000080000000000000007FFFFFFF";
-			rs3 <= X"000000007FFFFFFF000000007FFFFFFF"; 
-			
-			wait for 10 ns;
-			
-			--Testing for R4 Instruction: longMulSubLo
-			-- wordIn is 1 0 110 00000 00000 00000 00000
-			
-			--0th
-			--Normal Operation
-			--Set the 1st set of 64 bits of rs1 to 0x0000_0000_000F_4240, the lowest 64 bit number
-			--Set the 1st set of 32 bits of rs2 to 0x7FFF_FFFF, The highest 32 bit number
-			--Set the 1st set of 32 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
-			--That is: (2147483647 * 2147483647) + 1000000 = 4,611,686,014,131,420,609
-			--Expect: 3FFFFFFEFFF0BDC1
-			
-			--1st
-			--Normal Operation
-			--Set the 1st set of 64 bits of rs1 to 0x0000_0000_000F_4240, the lowest 64 bit number
-			--Set the 1st set of 32 bits of rs2 to 0x7FFF_FFFF, The highest 32 bit number
-			--Set the 1st set of 32 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
-			--That is: (2147483647 * 2147483647) - 1000000 = 4,611,686,014,133,420,609
-			--Expect: 3FFFFFFF000F4241
-			wordIn <= "1011000000000000000000000";
-			rs1 <= X"00000000000F424000000000000F4240";
-			rs2 <= X"000000007FFFFFFF000000007FFFFFFF";
-			rs3 <= X"000000007FFFFFFF000000007FFFFFFF"; 
-			
-			wait for 10 ns;
-			
-			--Testing for R4 Instruction: intMulAddHi
-			-- wordIn is 1 0 001 00000 00000 00000 00000
-			
-			--0th
-			--Set the 0th set of 32 bits of rs1 to 3  
-			--Set the 0th set of 32 bits of rs2 to 5 
-			--Set the 0th set of 32 bits of rs3 to 5
-			--That is: (5*5) + 3 = 28, which is 0x0_001C
-			
-			--1st
-			--Testing the overflow portion
-			--Set the 1st set of 32 bits of rs1 to 0x7FFF_FFFC, the hightest 32 bit number - 3
-			--Set the 1st set of 16 bits of rs2 to 0x7FFF, the highest 16 bit 
-			--Set the 1st set of 16 bits of rs3 to 0x0002, 2
-			--That is: (32,767*2) = 65,534 + 4,294,967,296 = 4,295,032,830, which overflowed
-			--Therefore, saturation should set it at 0x7FFF_FFFF
-			
-			--2nd
-			--Testing the normal operation
-			--Set the 1st set of 32 bits of rs1 to 0x1000_0000, the lowest 32 bit number
-			--Set the 1st set of 16 bits of rs2 to 0x0000, 0 
-			--Set the 1st set of 16 bits of rs3 to 0x0000, 0
-			--That is: (0*0) = 0 - 4,294,967,296 = -4,294,967,296, or 0x1000_0000
-			
-			--3rd
-			--Testing the normal operation
-			--Set the 1st set of 32 bits of rs1 to 0x7FFF_FFFF, the highest 32 bit number
-			--Set the 1st set of 16 bits of rs2 to 0x8000, The lowest 16 bit number 
-			--Set the 1st set of 16 bits of rs3 to 0x7FFF, The Highest 16 bit number
-			--That is: (32,767*-32,767) = -1,073,676,289 + 4,294,967,296 = ______
-			wordIn <= "1000100000000000000000000";
-			rs1 <= X"7FFFFFFF100000007FFFFFFC00000003";
-			rs2 <= X"80000000000000007FFF000000050000";
-			rs3 <= X"FFFF0000000000000002000000050000"; 
 			
 			wait for 10 ns;
 			
@@ -448,5 +351,153 @@ begin
 			rs3 <= X"7FFF00007FFF00008000000000050000"; 
 			
 			wait for 10 ns;
+			
+			--Testing for R4 Instruction: longMulAddLo
+			-- wordIn is 1 0 100 00000 00000 00000 00000
+			
+			--0th
+			--Overflow Operation
+			--Set the 0th set of 64 bits of rs1 to 0x7FFF_FFFF_FFFF_FFFF, the highest 64 bit number 
+			--Set the 0th set of 32 bits of rs2 to 0x7FFF_FFFF 
+			--Set the 0th set of 32 bits of rs3 to 0x7FFF_FFFF
+			--This should cap out at 0x7FFF_FFFF_FFFF_FFFF
+			
+			--1st
+			--Testing the Underflow portion
+			--Set the 1st set of 32 bits of rs1 to 0x8000_0000_0000_0000, the lowest 64 bit number
+			--Set the 1st set of 16 bits of rs2 to 0x8000_0000, The lowest 32 bit number
+			--Set the 1st set of 16 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
+			--This should saturate at 0x8000_0000_0000_0000
+			wordIn <= "1010000000000000000000000";
+			rs1 <= X"80000000000000007FFFFFFFFFFFFFFF";
+			rs2 <= X"0000000080000000000000007FFFFFFF";
+			rs3 <= X"000000007FFFFFFF000000007FFFFFFF"; 
+			
+			wait for 10 ns;
+			
+			--Testing for R4 Instruction: longMulAddLo
+			-- wordIn is 1 0 100 00000 00000 00000 00000
+			
+			--0th
+			--Normal Operation
+			--Set the 1st set of 64 bits of rs1 to 0x0000_0000_000F_4240, the lowest 64 bit number
+			--Set the 1st set of 32 bits of rs2 to 0x7FFF_FFFF, The highest 32 bit number
+			--Set the 1st set of 32 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
+			--That is: (2147483647 * 2147483647) + 1000000 = 4,611,686,014,133,420,609
+			--Expect: 3FFFFFFF000F4241
+			
+			--1st
+			--Normal Operation
+			--Set the 1st set of 64 bits of rs1 to 0x0000_0000_000F_4240, the lowest 64 bit number
+			--Set the 1st set of 32 bits of rs2 to 0x7FFF_FFFF, The highest 32 bit number
+			--Set the 1st set of 32 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
+			--That is: (2147483647 * 2147483647) + 1000000 = 4,611,686,014,133,420,609
+			--Expect: 3FFFFFFF000F4241
+			wordIn <= "1010000000000000000000000";
+			rs1 <= X"00000000000F424000000000000F4240";
+			rs2 <= X"000000007FFFFFFF000000007FFFFFFF";
+			rs3 <= X"000000007FFFFFFF000000007FFFFFFF"; 
+			
+			wait for 10 ns;
+			
+			--Testing for R4 Instruction: longMulAddHi
+			-- wordIn is 1 0 101 00000 00000 00000 00000
+			
+			--0th
+			--Normal Operation
+			--Set the 1st set of 64 bits of rs1 to 0x0000_0000_000F_4240, the lowest 64 bit number
+			--Set the 1st set of 32 bits of rs2 to 0x7FFF_FFFF, The highest 32 bit number
+			--Set the 1st set of 32 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
+			--That is: (2147483647 * 2147483647) + 1000000 = 4,611,686,014,133,420,609
+			--Expect: 3FFFFFFF000F4241
+			
+			--1st
+			--Normal Operation
+			--Set the 1st set of 64 bits of rs1 to 0x0000_0000_000F_4240, the lowest 64 bit number
+			--Set the 1st set of 32 bits of rs2 to 0x7FFF_FFFF, The highest 32 bit number
+			--Set the 1st set of 32 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
+			--That is: (2147483647 * 2147483647) + 1000000 = 4,611,686,014,133,420,609
+			--Expect: 3FFFFFFF000F4241
+			wordIn <= "1010100000000000000000000";
+			rs1 <= X"00000000000F424000000000000F4240";
+			rs2 <= X"7FFFFFFF000000007FFFFFFF00000000";
+			rs3 <= X"7FFFFFFF000000007FFFFFFF00000000"; 
+			
+			wait for 10 ns;
+			
+			--Testing for R4 Instruction: longMulAddHi
+			-- wordIn is 1 0 101 00000 00000 00000 00000
+			
+			--0th
+			--Overflow Operation
+			--Set the 0th set of 64 bits of rs1 to 0x7FFF_FFFF_FFFF_FFFF, the highest 64 bit number 
+			--Set the 0th set of 32 bits of rs2 to 0x7FFF_FFFF 
+			--Set the 0th set of 32 bits of rs3 to 0x7FFF_FFFF
+			--This should cap out at 0x7FFF_FFFF_FFFF_FFFF
+			
+			--1st
+			--Testing the Underflow portion
+			--Set the 1st set of 32 bits of rs1 to 0x8000_0000_0000_0000, the lowest 64 bit number
+			--Set the 1st set of 16 bits of rs2 to 0x8000_0000, The lowest 32 bit number
+			--Set the 1st set of 16 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
+			--This should saturate at 0x8000_0000_0000_0000
+			wordIn <= "1010100000000000000000000";
+			rs1 <= X"80000000000000007FFFFFFFFFFFFFFF";
+			rs2 <= X"80000000000000007FFFFFFF00000000";
+			rs3 <= X"7FFFFFFF000000007FFFFFFF00000000"; 
+			
+			wait for 10 ns;
+			
+			--Testing for R4 Instruction: longMulSubLo
+			-- wordIn is 1 0 110 00000 00000 00000 00000
+			
+			--0th
+			--Normal Operation
+			--Set the 0th set of 64 bits of rs1 to 0x7FFF_FFFF_FFFF_FFFF, the highest 64 bit number 
+			--Set the 0th set of 32 bits of rs2 to 0x7FFF_FFFF 
+			--Set the 0th set of 32 bits of rs3 to 0x7FFF_FFFF
+			--This should result in 0xBFFF_FFFF_0000_0002
+			
+			--1st
+			--Testing the Normal operation
+			--Set the 1st set of 32 bits of rs1 to 0x8000_0000_0000_0000, the lowest 64 bit number
+			--Set the 1st set of 16 bits of rs2 to 0x8000_0000, The lowest 32 bit number
+			--Set the 1st set of 16 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
+			--This should saturate to 0x4000 0000 8000 0000
+			wordIn <= "1011000000000000000000000";
+			rs1 <= X"80000000000000007FFFFFFFFFFFFFFF";
+			rs2 <= X"0000000080000000000000007FFFFFFF";
+			rs3 <= X"000000007FFFFFFF000000007FFFFFFF"; 
+			
+			wait for 10 ns;
+			
+			--Testing for R4 Instruction: longMulSubHi
+			-- wordIn is 1 0 111 00000 00000 00000 00000
+			
+			--0th
+			--Normal Operation
+			--Set the 1st set of 64 bits of rs1 to 0x0000_0000_000F_4240, the lowest 64 bit number
+			--Set the 1st set of 32 bits of rs2 to 0x7FFF_FFFF, The highest 32 bit number
+			--Set the 1st set of 32 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
+			--That is: (2147483647 * 2147483647) + 1000000 = 4,611,686,014,131,420,609
+			--Expect: 3FFFFFFEFFF0BDC1
+			
+			--1st
+			--Normal Operation
+			--Set the 1st set of 64 bits of rs1 to 0x0000_0000_000F_4240, the lowest 64 bit number
+			--Set the 1st set of 32 bits of rs2 to 0x7FFF_FFFF, The highest 32 bit number
+			--Set the 1st set of 32 bits of rs3 to 0x7FFF_FFFF, The highest 32 bit number
+			--That is: (2147483647 * 2147483647) - 1000000 = 4,611,686,014,133,420,609
+			--Expect: 3FFFFFFF000F4241
+			wordIn <= "1011100000000000000000000";
+			rs1 <= X"00000000000F424000000000000F4240";
+			rs2 <= X"7FFFFFFF000000007FFFFFFF00000000";
+			rs3 <= X"7FFFFFFF000000007FFFFFFF00000000"; 
+			
+			wait for 10 ns;
+			
+			
+			
+			finish;
         end process;
 end Behavioral;
