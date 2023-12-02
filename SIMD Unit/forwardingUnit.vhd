357746d0228178368bcdbfd0235726004eaea87e
Kyle Han <kyleh2420@gmail.com>
2023-12-02 17:21:46 -0500

    Fixed previous ALU update. Added fwd unit TB.

--- SIMD Unit/forwardingUnit.vhd
+++ SIMD Unit/forwardingUnit.vhd
@@ -27,26 +27,26 @@ use IEEE.NUMERIC_STD.ALL;
 entity fwdUnit is
 	Port(
 	--Inputs
-	--The instruction currently being executed
+	--The instruction currently being written back
+	wbInstruction: in std_logic_vector(24 downto 0);
+	--The instruction in the execution stage
 	exInstruction: in std_logic_vector(24 downto 0);
-	--The instruction in the instruction Fetch stage
-	ifInstruction: in std_logic_vector(24 downto 0);
 	
 	r3DataIn: in std_logic_vector(127 downto 0);
 	r2DataIn: in std_logic_vector(127 downto 0);
 	r1DataIn: in std_logic_vector(127 downto 0);
 	
-	--Data from the output of the ALU
-	exData: in std_logic_vector(127 downto 0);
+	--Data from the output of the wb register file
+	wbData: in std_logic_vector(127 downto 0);
 	
-	--Addresses of R3-R1 of the instruction fetch stage
-	ifR3Address: in std_logic_vector(4 downto 0);
-	ifR2Address: in std_logic_vector(4 downto 0);
-	ifR1Address: in std_logic_vector(4 downto 0);
-	ifRdAddress: in std_logic_vector(4 downto 0);
+	--Addresses of R3-R1 of the execute stage
+	exR3Address: in std_logic_vector(4 downto 0);
+	exR2Address: in std_logic_vector(4 downto 0);
+	exR1Address: in std_logic_vector(4 downto 0);
+	exRdAddress: in std_logic_vector(4 downto 0);
 	
 	--Address of Rd of the execute stage
-	exRdAddress: in std_logic_vector(4 downto 0);
+	wbRdAddress: in std_logic_vector(4 downto 0);
 	
 	
 	--Outputs
@@ -58,8 +58,11 @@ entity fwdUnit is
 end fwdUnit;
 
 architecture behavioral of fwdUnit is
-begin 
-	forward: process(exInstruction)
+begin
+	--For some reason, sensitivity list doesn't work with all
+	--Originally, I used wbInstruction and exInstruction as a part of the sensitivity list.
+	--Howev
+	forward: process(r3DataIn, r2DataIn, r1DataIn, wbData, exR3Address, exR2Address, exR1Address, exRdAddress, wbRdAddress)
 	begin
 		--By default, route the data right through.
 		r3DataOut <= r3DataIn;
@@ -67,32 +70,32 @@ begin
 		r1DataOut <= r1DataIn;
 		
 		--If the execute instruction is a nop, just forward the data through
-		--There is no exRd, so there's nothing to check.
-		if (exInstruction(24 downto 15) = "11----0000") then
+		--There is no wbRd, so there's nothing to check.
+		if (wbInstruction(24 downto 23) = "11" and wbInstruction(18 downto 15) = "0000") then
 			--Do nothing, just let the data forward through
 			
-		--If the instruction fetch opcode is a load imm, we'll compare exRd to garbage addresses.
+		--If the ex opcode is a load imm, we'll compare wbRd to garbage addresses.
 		--No data gets forwarded
-		elsif (ifInstruction(24) = '0') then
-		--However, if execute instruction is load imm, and so is the next one with the same rd, we have a hazard.
-			if (ifRdAddress = exRdAddress) then
-				rs1 <= exData;	
+		elsif (exInstruction(24) = '0') then
+		--However, if wb instruction is load imm, and ex is also li with same rd, we have a hazard.
+			if (exRdAddress = wbRdAddress) then
+				r1DataOut <= wbData;	
 			end if;
 		else 
 			--Handling any instruction into r3/r4
-			--If execute rd = if rs1, data hazard
-			if (exRdAddress = ifR1Address) then
-				r1DataOut <= exData;	
+			--If wb rd = ex rs1, data hazard
+			if (wbRdAddress = exR1Address) then
+				r1DataOut <= wbData;
 			end if;
 			
-			--If execute rd = if rs2, data hazard
-			if (exRdAddress = ifR2Address) then
-				r2DataOut <= exData;	
+			--If wb rd = ex rs2, data hazard
+			if (wbRdAddress = exR2Address) then
+				r2DataOut <= wbData;
 			end if;
 			
-			--If execute rd = if rs3, data hazard
-			if (exRdAddress = ifR3Address) then
-				r3DataOut <= exData;	
+			--If wb rd = ex rs3, data hazard
+			if (wbRdAddress = exR3Address) then
+				r3DataOut <= wbData;
 			end if;
 			
 		end if;		
